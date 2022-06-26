a8237bc3a6194dc36087341ac4abf211c41c0c19
18xiaolaohu <3190213847@qq.com>
2022-06-26 18:04:27 +0800

    完成登录功能

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,14 +1,108 @@
 <template>
-  <div>登录</div>
+  <div>
+    <!-- 顶部 -->
+    <van-nav-bar title="登录">
+      <!-- <van-icon name="cross" slot="left"/> -->
+      <template #left>
+        <van-icon name="cross" />
+      </template>
+    </van-nav-bar>
+    <van-form @submit="onSubmit" ref="form">
+      <van-field
+        v-model.trim="mobile"
+        name="mobile"
+        placeholder="手机号"
+        :rules="[
+          { required: true, message: '请填写手机号' },
+          { pattern: /^(?:(?:\+|00)86)?1\d{10}$/, message: '不符合手机的格式' },
+        ]"
+      >
+        <template #left-icon>
+          <i class="toutiao toutiao-shouji"></i>
+        </template>
+      </van-field>
+      <van-field
+        v-model.trim="code"
+        type="password"
+        name="code"
+        placeholder="验证码"
+        :rules="[
+          { required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '验证码长度必须为6位' },
+        ]"
+      >
+        <template #left-icon>
+          <i class="toutiao toutiao-yanzhengma"></i>
+        </template>
+        <template #button>
+          <van-count-down
+            v-if="isCountDownShow"
+            :time="time"
+            format="ss s"
+            @finish="isCountDownShow = false"
+          />
+          <van-button
+            @click="onSendSms"
+            v-else
+            size="small"
+            class="yzm"
+            native-type="button"
+            >发送验证码</van-button
+          >
+        </template>
+      </van-field>
+      <div style="margin: 16px">
+        <van-button round block native-type="submit" class="login-btn"
+          >提交
+        </van-button>
+      </div>
+    </van-form>
+  </div>
 </template>
 
 <script>
+import { getSmsCode, login } from '@/api/user'
 export default {
   created () { },
   data () {
-    return {}
+    return {
+      mobile: '13911111111', // 手机号
+      code: '246810', // 短信验证码
+      time: 60 * 1000, // time 属性表示倒计时总时长，单位为毫秒。
+      isCountDownShow: false
+    }
   },
-  methods: {},
+  methods: {
+    async onSubmit (values) {
+      // console.log('submit', values)
+      try {
+        const res = await login(values)
+        // console.log(res)
+        this.$store.commit('setUser', res.data.data)
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    async onSendSms () {
+      try {
+        await this.$refs.form.validate('mobile')
+        // console.log('校验通过')
+        this.isCountDownShow = true
+        try {
+          await getSmsCode(this.mobile)
+          this.$toast.success('发送成功')
+        } catch (err) {
+          console.log(err)
+          this.$toast.fail('发送失败，请重试')
+        }
+      } catch (err) {
+        console.log(err)
+        console.log('校验失败')
+        // 轻提示
+        this.$toast.fail('手机格式不对')
+      }
+    }
+  },
   computed: {},
   watch: {},
   filters: {},
@@ -16,5 +110,36 @@ export default {
 }
 </script>
 
-<style scoped lang='less'>
+<style scoped lang="less">
+.toutiao {
+  font-size: 37px;
+}
+.yzm {
+  width: 152px;
+  height: 46px;
+  line-height: 46px;
+  background-color: #ededed;
+  border-radius: 23px;
+  font-size: 22px;
+  color: #666;
+  .van-button__text {
+    zoom: 0.96;
+  }
+}
+.login-btn {
+  width: 694px;
+  height: 88px;
+  background-color: #6db4fb;
+  border-radius: 10px;
+  font-size: 30px;
+  color: #fff;
+}
+.van-count-down {
+  position: fixed;
+  right: 20px;
+}
+.yzm {
+  position: fixed;
+  right: 20px;
+}
 </style>
