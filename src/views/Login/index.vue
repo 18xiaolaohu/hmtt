8eb1b8294713dc05b49bf340e1fa69d4ca28d302
18xiaolaohu <3190213847@qq.com>
2022-06-26 11:44:23 +0800

    路由配置完成

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,13 +1,9 @@
 <template>
-  <div>
-     <!-- 一级占位符 -->
-    <router-view></router-view>
-  </div>
+  <div>登录</div>
 </template>
 
 <script>
 export default {
-
   created () { },
   data () {
     return {}
